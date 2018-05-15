91cc71d39f3f243eda73ab0a8adbe938c3572681
harrimand <harrimand@gmail.com>
2018-05-15 11:17:16 -0700

    updated gtkWidgets

--- gtkWidgets.py
+++ gtkWidgets.py
@@ -70,7 +70,7 @@ class UserInput(Gtk.Window):
                     " 7 ", " 8 ", " 9 ", " + ",
                     " 4 ", " 5 ", " 6 ", " - ",
                     " 1 ", " 2 ", " 3 ", " \\ ",
-                    " * ", " 0 ", " # ", " ^2"
+                    " . ", " 0 ", " # ", " ^2"
                     ]
         cols = 4
 
@@ -79,27 +79,43 @@ class UserInput(Gtk.Window):
             kp.connect("clicked", self.key_pressed)
             self.keypad.attach(kp, i % cols + 4, i // cols, 1, 1)
 
+        # Add Image to vboxL
+        self.eventbox = Gtk.EventBox()
+        self.eventbox.connect("button-press-event", self.image_clicked)
+        self.image = Gtk.Image()
+        self.image.set_from_file("phylo.png")
+        self.eventbox.add(self.image)
+        vboxL.pack_start(self.eventbox, False, False, 0)
+
+    # Append entry1 and entry1 when 'Add Text' button clicked
     def add_text(self, widget):
         end_iter = self.textbuffer.get_end_iter()
         self.textbuffer.insert(end_iter, "\n" + self.text1.get_text())
         end_iter = self.textbuffer.get_end_iter()
         self.textbuffer.insert(end_iter, "\t" + self.text2.get_text())
 
+    # Append window size when 'Do Something' button clicked
     def do_something(self, widget):
         W, H = self.get_size()
         end_iter = self.textbuffer.get_end_iter()
-        self.textbuffer.insert(end_iter, "\nWindow Size: " + str(W) + " x " + str(H))
+        self.textbuffer.insert(end_iter, "\nWindow Size: " + str(W) + " x " + str(H) + "\n")
 
+    # Append keypad button label
     def key_pressed(self, widget):
         end_iter = self.textbuffer.get_end_iter()
-        self.textbuffer.insert(end_iter, widget.get_label())
+        label = widget.get_label().strip()
+        if label.isdigit() or label in "ABCDEF.":
+            self.textbuffer.insert(end_iter, label)
+        else: self.textbuffer.insert(end_iter, " " + label + " ")
 
-
-
+    # Append Mouse Coordinates when the image is clicked.
+    def image_clicked(self, widget, event):
+        end_iter = self.textbuffer.get_end_iter()
+        xy = " X: {0:.2f}   Y: {1:.2f}".format(event.x, event.y)
+        self.textbuffer.insert(end_iter, "\n\tDon't touch the image!!\t" + xy)
 
 window = UserInput()
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()
 Gtk.main()
-
 
