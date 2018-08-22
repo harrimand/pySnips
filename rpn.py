300f2d06f81e6cc4f795eec6dcfe8f42e6937bc9
harrimand <harrimand@gmail.com>
2018-08-21 20:35:17 -0700

    Updated documentation

--- rpn.py
+++ rpn.py
@@ -29,14 +29,15 @@ class rpn:
         self.update(inStr)
 
     def update(self, inStr):
+        """Parse input, push integers and floats or perfrom operation """    
         for n in inStr.split():
             if self.isnum(n):
                 if '.' in n:
                     self.stack.append(float(n))
-                    print("Push".ljust(15), n.ljust(15), self.stack)
+                    print("Push".ljust(15), n.ljust(30), self.stack)
                 else:
                     self.stack.append(int(n))
-                    print("Push".ljust(15), n.ljust(15), self.stack)
+                    print("Push".ljust(15), n.ljust(30), self.stack)
             else:
                 if n in self.funs:
                     self.funs[n]()
@@ -65,122 +66,137 @@ class rpn:
             ")
 
     def getStack(self):
+        """Return list of stack contents"""
         return self.stack
 
     def isnum(self, n):
         return n.replace('.','',1).isdigit()
 
     def add(self):
+        """Add Level 2 to Level 1 and place result on stack"""
         if len(self.stack) > 1:
             n2 = self.stack.pop()
             n1 = self.stack.pop()
             self.stack.append(n1 + n2)
-            print("Add".ljust(15), (str(n1) + ' + ' + str(n2)).ljust(15), self.stack)
+            print("Add".ljust(15), (str(n1) + ' + ' + str(n2)).ljust(30), self.stack)
         else:
             print("Add requires 2 items on Stack")
 
     def mul(self):
+        """Multiply Level 2 by Level 1 and place result on stack"""
         if len(self.stack) > 1:
             n2 = self.stack.pop()
             n1 = self.stack.pop()
             self.stack.append(n1 * n2)
-            print("Multiply".ljust(15), (str(n1) + ' * ' + str(n2)).ljust(15), self.stack)
+            print("Multiply".ljust(15), (str(n1) + ' * ' + str(n2)).ljust(30), self.stack)
         else:
             print("Multiply requires 2 items on Stack")
 
     def sub(self):
+        """Subtract Level 1 from Level 2 and place result on stack"""
         if len(self.stack) > 1:
             n2 = self.stack.pop()
             n1 = self.stack.pop()
             self.stack.append(n1 - n2)
-            print("Subtract".ljust(15), (str(n1) + ' - ' + str(n2)).ljust(15), self.stack)
+            print("Subtract".ljust(15), (str(n1) + ' - ' + str(n2)).ljust(30), self.stack)
         else:
             print("Subtract requires 2 items on Stack")
 
     def div(self):
+        """Divide Level 2 by Level 1 and place result on stack"""
         if len(self.stack) > 1:
             n2 = self.stack.pop()
             n1 = self.stack.pop()
             self.stack.append(n1 / n2)
-            print("Divide".ljust(15), (str(n1) + ' / ' + str(n2)).ljust(15), self.stack)
+            print("Divide".ljust(15), (str(n1) + ' / ' + str(n2)).ljust(30), self.stack)
         else:
             print("Divide requires 2 items on Stack")
 
     def dup(self):
+        """Duplicate Level 1 on the stack"""
         if len(self.stack) > 0:
             n1 = self.stack.pop()
             self.stack.append(n1)
             self.stack.append(n1)
-            print("Duplicate".ljust(15), str(n1).ljust(15), self.stack)
+            print("Duplicate".ljust(15), str(n1).ljust(30), self.stack)
         else:
             print("Stack Empty")
 
     def dup2(self):
+        """Duplicate Level 2 and Level 1 on the stack"""
         if len(self.stack) > 1:
             self.stack = self.stack + self.stack[-2:]
             dupStr = " ".join([str(i) for i in self.stack[-2:]])
-            print("Dup 2".ljust(15), dupStr.ljust(15), self.stack)
+            print("Dup 2".ljust(15), dupStr.ljust(30), self.stack)
         else:
             print("dup2 requires 2 items on the stack")
 
     def sq(self):
+        """Square Level 1 and place result on stack"""
         if len(self.stack) > 0:
             n1 = self.stack.pop()
             self.stack.append(n1 * n1)
-            print("Square".ljust(15), str(n1).ljust(15), self.stack)
+            print("Square".ljust(15), str(n1).ljust(30), self.stack)
         else:
             print("Stack Empty")
 
     def sqr(self):
+        """Square Root Level 1 and place result on stack"""
         if len(self.stack) > 0:
             n1 = self.stack.pop()
             self.stack.append(math.sqrt(n1))
-            print("Square Root".ljust(15), str(n1).ljust(15), self.stack)
+            print("Square Root".ljust(15), str(n1).ljust(30), self.stack)
         else:
             print("Stack Empty")
 
     def power(self):
+        """Raise Level 2 to power of Level 1 and place result on stack"""
         if len(self.stack) > 1:
             n1 = self.stack.pop()
             n2 = self.stack.pop()
             self.stack.append(n2**n1)
-            print("Power".ljust(15), (str(n1) + ' ^ ' + str(n2)).ljust(15), self.stack)
+            print("Power".ljust(15), (str(n2) + ' ^ ' + str(n1)).ljust(30), self.stack)
         else:
             print("Power requires 2 items on Stack")
 
     def mod(self):
+        """Level 2 modulo Level 1 and place result on stack"""
         if len(self.stack) > 1:
             n1 = self.stack.pop()
             n2 = self.stack.pop()
             self.stack.append(n2%n1)
-            print("Modulo".ljust(15), (str(n2) + ' % ' + str(n1)).ljust(15), self.stack)
+            print("Modulo".ljust(15), (str(n2) + ' % ' + str(n1)).ljust(30), self.stack)
         else:
             print("Modulo requires 2 items on Stack")
 
     def pick(self):
+        """Pick item from stack selected by Level 1 value + 1"""
         n1 = self.stack.pop()
         if len(self.stack) > (n1 - 2):
-            n2 = self.stack[len(self.stack) - n1 + 1]
+            n2 = self.stack[len(self.stack) - n1]
             self.stack.append(n2)
-            print("Pick Level".ljust(15), str(n1).ljust(15), self.stack)
+            print("Pick Level".ljust(15), str(n1).ljust(30), self.stack)
         else:
             self.stack.append(n1)
             print("Pick Level out of range")
 
     def pi(self):
+        """Place Pi on stack """
         n1 = math.pi
         self.stack.append(n1)
-        print("Push Pi".ljust(15), (str(n1)[:4]+"...").ljust(15), self.stack)
+        print("Push Pi".ljust(15), (str(n1)[:4]+"...").ljust(30), self.stack)
 
     def inv(self):
+        """1 / Level 1 Place result on stack"""
         if len(self.stack) > 0:
             n1 = self.stack.pop()
             self.stack.append(1 / n1)
-            print("Invert".ljust(15), ("1/"+str(n1)).ljust(15), self.stack)
+            print("Invert".ljust(15), ("1/"+str(n1)).ljust(30), self.stack)
         else:
             print("Stack Empty")
 
     def show(self):
+        """Print columns with Level #s and Level Values"""
         l = len(self.stack)
         i = 0
         for L in range(l, 0, -1):
@@ -188,22 +204,25 @@ class rpn:
             i += 1
 
     def drop(self):
+        """Drop Level 1 from stack"""
         if len(self.stack) > 0:
-            self.stack.pop()
+            print("Drop".ljust(15), str(self.stack.pop()).ljust(30), self.stack)
+#           self.stack.pop()
         else:
             print("Stack Empty")
 
     def swap(self):
+        """Swap Level 1 with Level 2"""
         if len(self.stack) > 1:
             n1 = self.stack.pop()
             n2 = self.stack.pop()
             self.stack.append(n1)
             self.stack.append(n2)
-            print("Swap".ljust(15), "L1 <-> L2".ljust(15), self.stack)
+            print("Swap".ljust(15), "L1 <-> L2".ljust(30), self.stack)
         else:
             print("Swap requires 2 items on Stack")
 
     def clr(self):
+        """Clear (empty) stack"""
         self.stack = []
-        print("Clear Stack".ljust(15), "clr".ljust(15), self.stack)
-
+        print("Clear Stack".ljust(15), "clr".ljust(30), self.stack)
