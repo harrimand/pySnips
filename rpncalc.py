bc6cc6c8aa0bc70d77284a732903aaba1d191268
harrimand <harrimand@gmail.com>
2018-08-22 12:52:21 -0700

    Updated GUI ScrollWindow

--- rpncalc.py
+++ rpncalc.py
@@ -1,218 +1,72 @@
-#!/bin/python3
-# RPN Calculator by Darrell Harriman  harrimand@gmail.com
 
-import math
-class rpn:
-    def __init__(self, inStr=''):
-        """Create RPN Stack from string"""
-        self.stack = []
-        self.funs = {
-            "h": self.help,
-            "+": self.add,
-            "*": self.mul,
-            "-": self.sub,
-            "/": self.div,
-            "du": self.dup,
-            "sq": self.sq,
-            "sqr": self.sqr,
-            "^": self.power,
-            "%": self.mod,
-            "p": self.pick,
-            "pi": self.pi,
-            "1/": self.inv,
-            "sh": self.show,
-            "dr": self.drop,
-            "sw": self.swap
-        }
-        self.update(inStr)
+import gi
+gi.require_version('Gtk', '3.0')
 
-    def update(self, inStr):
-        for n in inStr.split():
-            if self.isnum(n):
-                if '.' in n:
-                    self.stack.append(float(n))
-                    print("Push".ljust(15), n.ljust(15), self.stack)
-                else:
-                    self.stack.append(int(n))
-                    print("Push".ljust(15), n.ljust(15), self.stack)
-            else:
-                if n in self.funs:
-                    self.funs[n]()
-        print()
+from gi.repository import Gtk, Pango
+from rpn import rpn
 
-    def help(self):
-        print("\n\
-            h    display this help\n\
-            +    add Level 1 to Level 2\n\
-            *    muliply Level 1 by Level 2\n\
-            -    subract Level 1 from Level 2\n\
-            /    divide Level 2 by Level 1\n\
-            du   duplicate Level 1\n\
-            sq   square of Level 1\n\
-            sqr  square root of Level 1\n\
-            ^    Level 2 raised to power of Level 1\n\
-            %    Level 2 modulo Level 1\n\
-            p    pick Level defined by Level 1 value\n\
-            pi   push Pi to stack\n\
-            1/   invert (1/x) Level 1\n\
-            sh   Print stack with Level numbers\n\
-            dr   drop Level 1 from stack\n\
-            sw   swap Level 1 and Level 2\n\
-            ")
+class UserInput(Gtk.Window, rpn):
 
-    def getStack(self):
-        return self.stack
+    def __init__(self):
+        Gtk.Window.__init__(self, title="RPN Calculator")
+        self.set_border_width(10)
+        self.set_size_request(600, 500)
+        self.set_position(Gtk.WindowPosition.CENTER)
 
-    def isnum(self, n):
-        return n.replace('.','',1).isdigit()
+        # Layout
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.add(vbox)
 
-    def add(self):
-        if len(self.stack) > 1:
-            n2 = self.stack.pop()
-            n1 = self.stack.pop()
-            self.stack.append(n1 + n2)
-            print("Add".ljust(15), (str(n1) + ' + ' + str(n2)).ljust(15), self.stack)
+        # Scroll Window (Packed in right side of hbox)
+        self.scrolledwindow = Gtk.ScrolledWindow()
+        self.scrolledwindow.set_hexpand(True)
+        self.scrolledwindow.set_vexpand(True)
+        vbox.pack_start(self.scrolledwindow, True, True, 0)
+
+        # TextView added to Scroll Window
+        self.textview = Gtk.TextView()
+        font = Pango.FontDescription('monospace 14')
+        self.textview.modify_font(font)
+        self.textbuffer = self.textview.get_buffer()
+        self.textbuffer.set_text("RPN Calculator by Darrell Harriman\
+        \n\n\tEnter h for help\n\n")
+        self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.scrolledwindow.add(self.textview)
+
+        # Text Entry 1
+        self.text1 = Gtk.Entry()
+        self.text1.set_text("")
+        self.text1.connect("activate", self.submit_entry1)
+        vbox.pack_start(self.text1, False, False, 0)
+
+        self.R = rpn("")
+        self.text1.grab_focus()
+
+# Update stack and textview when 'Enter' pressed 
+    def submit_entry1(self, widget):
+        rpnStr = self.text1.get_text()
+        if rpnStr == '?':
+            self.textbuffer.set_text(self.R.help())
+            self.text1.set_text("")
+        elif rpnStr.startswith("exit"):
+            Gtk.main_quit()
         else:
-            print("Add requires 2 items on Stack")
+            self.R.update(rpnStr)
+            rStr = self.R.getStack()
+            # end_iter = self.textbuffer.get_end_iter()
+            self.textbuffer.set_text(rStr)
+            # self.textbuffer.insert(end_iter, "\n" + rpnStr)
+            self.text1.set_text("")
+#            adj = self.scrolledwindow.get_vadjustment()
+#            adj.set_value(adj.upper - adj.page_size)
 
-    def mul(self):
-        if len(self.stack) > 1:
-            n2 = self.stack.pop()
-            n1 = self.stack.pop()
-            self.stack.append(n1 * n2)
-            print("Multiply".ljust(15), (str(n1) + ' * ' + str(n2)).ljust(15), self.stack)
-        else:
-            print("Multiply requires 2 items on Stack")
-
-    def sub(self):
-        if len(self.stack) > 1:
-            n2 = self.stack.pop()
-            n1 = self.stack.pop()
-            self.stack.append(n1 - n2)
-            print("Subtract".ljust(15), (str(n1) + ' - ' + str(n2)).ljust(15), self.stack)
-        else:
-            print("Subtract requires 2 items on Stack")
-
-    def div(self):
-        if len(self.stack) > 1:
-            n2 = self.stack.pop()
-            n1 = self.stack.pop()
-            self.stack.append(n1 / n2)
-            print("Divide".ljust(15), (str(n1) + ' / ' + str(n2)).ljust(15), self.stack)
-        else:
-            print("Divide requires 2 items on Stack")
-
-    def dup(self):
-        if len(self.stack) > 0:
-            n1 = self.stack.pop()
-            self.stack.append(n1)
-            self.stack.append(n1)
-            print("Duplicate".ljust(15), str(n1).ljust(15), self.stack)
-        else:
-            print("Stack Empty")
-
-    def sq(self):
-        if len(self.stack) > 0:
-            n1 = self.stack.pop()
-            self.stack.append(n1 * n1)
-            print("Square".ljust(15), str(n1).ljust(15), self.stack)
-        else:
-            print("Stack Empty")
-
-    def sqr(self):
-        if len(self.stack) > 0:
-            n1 = self.stack.pop()
-            self.stack.append(math.sqrt(n1))
-            print("Square Root".ljust(15), str(n1).ljust(15), self.stack)
-        else:
-            print("Stack Empty")
-
-    def power(self):
-        if len(self.stack) > 1:
-            n1 = self.stack.pop()
-            n2 = self.stack.pop()
-            self.stack.append(n2**n1)
-            print("Power".ljust(15), (str(n1) + ' ^ ' + str(n2)).ljust(15), self.stack)
-        else:
-            print("Power requires 2 items on Stack")
-
-    def mod(self):
-        if len(self.stack) > 1:
-            n1 = self.stack.pop()
-            n2 = self.stack.pop()
-            self.stack.append(n2%n1)
-            print("Modulo".ljust(15), (str(n2) + ' % ' + str(n1)).ljust(15), self.stack)
-        else:
-            print("Modulo requires 2 items on Stack")
-
-    def pick(self):
-        n1 = self.stack.pop()
-        if len(self.stack) > (n1 - 2):
-            n2 = self.stack[len(self.stack) - n1 + 1]
-            self.stack.append(n2)
-            print("Pick Level".ljust(15), str(n1).ljust(15), self.stack)
-        else:
-            self.stack.append(n1)
-            print("Pick Level out of range")
-
-    def pi(self):
-        n1 = math.pi
-        self.stack.append(n1)
-        print("Push Pi".ljust(15), (str(n1)[:4]+"...").ljust(15), self.stack)
-
-    def inv(self):
-        if len(self.stack) > 0:
-            n1 = self.stack.pop()
-            self.stack.append(1 / n1)
-            print("Invert".ljust(15), ("1/"+str(n1)).ljust(15), self.stack)
-        else:
-            print("Stack Empty")
-
-    def show(self):
-        l = len(self.stack)
-        i = 0
-        for L in range(l, 0, -1):
-            print(str(L).rjust(8), "\t", self.stack[i])
-            i += 1
-
-    def drop(self):
-        if len(self.stack) > 0:
-            self.stack.pop()
-        else:
-            print("Stack Empty")
-
-    def swap(self):
-        if len(self.stack) > 1:
-            n1 = self.stack.pop()
-            n2 = self.stack.pop()
-            self.stack.append(n1)
-            self.stack.append(n2)
-            print("Swap".ljust(15), "L1 <-> L2".ljust(15), self.stack)
-        else:
-            print("Swap requires 2 items on Stack")
+            # it = self.textbuffer.get_iter_at_line(self.textbuffer.get_line_count())
+            # self.textview.scroll_to(it)
 
 
+window = UserInput()
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+Gtk.main()
 
-R = rpn("h 1 2 3 4 5")
-
-R.update("sh 4 sh p sh 7 sh p sh sw sh")
-
-"""
-R.update("sh")
-R.update("4")
-R.update("sh")
-R.update("p")
-R.update("sh")
-R.update("7")
-R.update("sh")
-R.update("p")
-R.update("sh")
-R.update("sw")
-R.update("sh")
-"""
-
-#R = rpn(" 2 1 + 4.5 * 5 * 2 / 7 + 7 8 9 + 2 sq * * du du 169 sqr 6 p pi 27 du + 4 * 13 14 15 sh")
-#P = rpn("3 sq 4 sq + sqr pi * 1/")
-
-print(R.getStack())
 
