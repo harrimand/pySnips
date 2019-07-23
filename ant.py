28c9fc0c0e2d23c897be53f8bafac61830f5a4d5
harrimand <harrimand@gmail.com>
2019-07-23 09:35:35 -0700

    First Commit

---
+++ ant.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from turtle import *
+from random import randint
+
+T = Turtle()
+Tsc = T.getscreen()
+Tsc.setup(800, 800)
+Tsc.bgcolor("light green")
+
+T.up()
+T.setposition(-350, 350)
+T.setheading(0)
+T.width(2)
+T.down()
+T.color("blue", "yellow")
+T.begin_fill()
+
+for s in range(4):
+    T.fd(700)
+    T.right(90)
+
+T.end_fill()
+T.up()
+T.home()
+T.down()
+
+for s in range(2000):
+    Th = T.heading()
+    T.setheading(Th + randint(-30, 30))
+    T.fd(5)
+    if (abs(T.xcor()) >= 344):
+        T.setheading((180 - Th) % 360)
+    if (abs(T.ycor()) >= 344):
+        T.setheading((360 - Th))
+
+Tsc.exitonclick()
+
