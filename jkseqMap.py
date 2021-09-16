781acab36a6fb6a13d8653c3a99c8736070c29c6
harrimand <harrimand@gmail.com>
2021-09-16 11:09:09 -0700

    Update jkseqMap.py

--- jkseqMap.py
+++ jkseqMap.py
@@ -1,14 +1,15 @@
-
 from jkseq import *
 from kmap import *
 
+gcs = [n ^ n>>1 for n in range(16)]
 
+m = lambda S: [S[:4]]+[S[7:3:-1]]+[S[8:12]]+[S[15:11:-1]]
 
+# Qseq = [[jkdF[[Qn]].loc[n].tolist()[0] for n in seq] for Qn in jkCols]
 
-Qseq = [[jkdF[[Qn]].loc[n].tolist()[0] for n in seq] for Qn in jkCols]
-KM = [karnaughMap(Q, 10) for Q in Qseq] 
+Qseq = [[jkdF.loc[n, Qn][0] for n in gcs] for Qn in jkCols]
 
-
+KM = [karnaughMap(Q, 10) for Q in Qseq]
 
 for i in range(0,7,2):
     print('\n\t', jkCols[i], ' '*40, jkCols[i+1])
@@ -24,16 +25,11 @@ for i, KK in enumerate(KM):
 
 print('\n\n')
 
-
 '''
 KMAP = karnaughMap(k0seq, 10)
-
 print('\n\n\tK0')
-
 for K in KMAP:
     print(K)
-
 print('\n\n')
 '''
-
 
