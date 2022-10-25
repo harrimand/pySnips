18f0f402d8a9d1da94fa8b1bad4d43408e977ef3
harrimand <24970005+harrimand@users.noreply.github.com>
2022-10-25 12:42:44 -0700

    Added Functions

--- primegen.py
+++ primegen.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
 
 from math import sqrt
 import csv
+from os import system
+
+cls = lambda: system("clear")
 
 def isprime(m):
     for n in range(3, int(sqrt(m))+1, 2):
@@ -13,6 +17,17 @@ def primeList(max):
     for n in range(3, max, 2):
         if isprime(n):
             L.append(n)
+    return L
+
+def primeCount(count):
+    cnt = 1
+    L = [2]
+    p = 3
+    while cnt < count:
+        if isprime(p):
+            L.append(p)
+            cnt += 1
+        p += 2
     return L
 
 def csvw(Primes):
@@ -34,7 +49,7 @@ def primeaddends(N):
                 adds.append((Pints[m], N - Pints[m]))
             m += 1
         return adds
-
+    
 def primeadds(N):
     pradds = {}
     for n in range(4, N, 2):
@@ -43,19 +58,21 @@ def primeadds(N):
             pradds[n] = a
     return pradds
 
-MaxN = 400
+consecs = lambda p: [(c, p[i+1]) for i, c in enumerate(p[:-1]) if c == (p[i + 1] - 2)]
 
-Pints = primeList(MaxN)
-Primes = [[str(P)] for P in Pints]
-print(Pints)
-# print(str(Pints[50]))
-print("Length: ", len(Primes))
+if __name__ == "__main__":
+    MaxN = 400
+    Pints = primeList(MaxN)
+    Primes = [[str(P)] for P in Pints]
+    print(Pints)
+    # print(str(Pints[50]))
+    print("Length: ", len(Primes))
 
-csvw(Primes)
+    csvw(Primes)
 
-pradds = primeadds(MaxN)
-for k in pradds.keys():
-    print(k, pradds[k])
+    pradds = primeadds(MaxN)
+    for k in pradds.keys():
+        print(k, pradds[k])
 
-# print(pradds)
-
+    # print(pradds)
+    
