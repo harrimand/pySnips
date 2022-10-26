20bc83a84d4868877c50527d3f9db86cc4c9facf
harrimand <24970005+harrimand@users.noreply.github.com>
2022-10-26 09:19:08 -0700

    Added Prime Difference Function

--- primegen.py
+++ primegen.py
@@ -12,9 +12,17 @@ def isprime(m):
             return False
     return True
 
-def primeList(max):
+def primeList(mx):
     L = [2]
-    for n in range(3, max, 2):
+    for n in range(3, mx, 2):
+        if isprime(n):
+            L.append(n)
+    return L
+
+def primeRange(begin, end):
+    begin = begin + ((begin + 1) % 2)
+    L = []
+    for n in range(begin, end + 1, 2):
         if isprime(n):
             L.append(n)
     return L
@@ -49,7 +57,7 @@ def primeaddends(N):
                 adds.append((Pints[m], N - Pints[m]))
             m += 1
         return adds
-    
+
 def primeadds(N):
     pradds = {}
     for n in range(4, N, 2):
@@ -57,6 +65,26 @@ def primeadds(N):
         if len(a) > 0:
             pradds[n] = a
     return pradds
+
+diffs = lambda pc: [pc[i+1]-n for i, n in enumerate(pc[:-1])]
+ndiffs = lambda d: {n: d.count(n) for n in set(d)}
+
+
+def primeHelp():
+    helpstr = "\n\
+        isprime(N):  Return true if N is prime.  False otherwise\n\
+        primeList(mx): Return all prime numbers up to mx\n\
+        primeRange(begin, end): Return list of primes between begin and end inclusive\n\
+        primeCount(N): Return list containing quantity N prime numbers\n\
+        csvw(Primes): Write Primes list to CSV file\n\
+        binw(Primes): Write Primes list to BIN file\n\
+        primeaddends(N): Helper Function to be called by primeadds(N)\n\
+        primeadds(N): Return list of pairs of prime numbers that add up\n\
+             to all even numbers > 2 & < N\n\
+        consecs(Primes): Return list of tuples of prime numbers differing by 2\n\
+        diffs(Primes): Return list of differences between prime numbers in list\n\
+        ndiffs(diffs(Primes)): Return dictionary with keys = diffs, values = counts\n"
+    print("\n", helpstr)
 
 consecs = lambda p: [(c, p[i+1]) for i, c in enumerate(p[:-1]) if c == (p[i + 1] - 2)]
 
@@ -75,4 +103,4 @@ if __name__ == "__main__":
         print(k, pradds[k])
 
     # print(pradds)
-    
+
